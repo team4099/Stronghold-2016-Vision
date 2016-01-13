975f4c68ebfd6180c88e4b5e3278efad7e9bf9ed
Parth Oza <plusparth@gmail.com>
2016-01-13 15:37:53 -0500

    Added contour detection

--- kinect_video.py
+++ kinect_video.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 import freenect
-import cv
+import cv2
 import frame_convert
+import time
+import vision_processing
+import numpy
 
 lookup_table_depth = []
 
 
-cv.NamedWindow('Depth')
-cv.NamedWindow('Video')
+# cv.NamedWindow('Depth')
+cv2.namedWindow('Video')
+cv2.namedWindow('Thresholded')
+
 print('Press ESC in window to stop')
 
 
@@ -27,19 +32,35 @@ def get_depth():
 
 
 def get_video():
-    return frame_convert.video_cv(freenect.sync_get_video()[0])
+    video_data = freenect.sync_get_video()
+    return video_data[1], frame_convert.video_cv(video_data[0])
 
 
 def generate_lookup_table():
     for i in range(2048):
         lookup_table_depth.append(1/(i * -0.0030711016 + 3.3309495161))
 
+
 generate_lookup_table()
 
-# while 1:
-    # cv.ShowImage('Depth', get_depth())
-
-cv.ShowImage('Video', get_video())
 while 1:
-    if cv.WaitKey(10) == 27:
-        break
+    # cv.ShowImage('Depth', get_depth())
+    ret, video_frame = get_video()
+    # print type(video_frame)
+    video_frame = numpy.asarray(video_frame[:,:]) # access as mat
+    # print video_frame.shape
+    if video_frame.size <= 10:
+        continue
+    # print type(video_frame)
+    thresholded_image = vision_processing.threshold_image_for_tape(video_frame)
+    cv2.imshow('Video', video_frame)
+    cv2.imshow('Thresholded', thresholded_image)
+    save = raw_input("Save?")
+    if save.upper() == "EXIT":
+        break;
+    if "V" in save.upper():
+        cv2.imwrite("video_" + str(int(time.time()/100)) + ".png",
+                    video_frame)
+    if "T" in save.upper():
+        cv2.imwrite("thresholded_" + str(int(time.time()/100)) + ".png",
+                    thresholded_image)
--- vision_processing.py
+++ vision_processing.py
@@ -1,1 +1,44 @@
-#!/usr/bin/python
+import cv2
+import numpy
+
+B_RANGE = (235, 255)
+G_RANGE = (235, 255)
+R_RANGE = (235, 255)
+
+# cv.NamedWindow("Vision")
+
+def threshold_image_for_tape(orig_image):
+    # print orig_image.size
+    orig_image = cv2.medianBlur(orig_image, 5)
+    height, width = orig_image.shape[0], orig_image.shape[1]
+    eight_bit_image = numpy.zeros((height, width, 1), numpy.uint8)
+    cv2.inRange(orig_image,
+                (B_RANGE[0], G_RANGE[0], R_RANGE[0], 0),
+                (B_RANGE[1], G_RANGE[1], R_RANGE[1], 100),
+                eight_bit_image)
+    # eight_bit_image = cv2.adaptiveThreshold(orig_image,
+    #                             255,
+    #                             cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    #                             cv2.THRESH_BINARY,
+    #                             8,
+    #                             0)
+    cv2.blur(eight_bit_image, (7, 7))
+    return eight_bit_image
+
+# def canny_edge_detection(orig_image):
+def get_contours(orig_image):
+    contours, hierarchy = cv2.findContours(orig_image,
+                                            cv2.RETR_EXTERNAL,
+                                            cv2.CHAIN_APPROX_NONE)
+    new_image = orig_image
+    cv2.drawContours(new_image, contours, -1, (255, 255, 255))
+    return new_image
+
+image_to_process = cv2.imread("backboard_bright_bg.jpg")
+# print type(image_to_process)
+thresholded_image = threshold_image_for_tape(image_to_process)
+cv2.imwrite("thresholded.png", thresholded_image)
+contoured_image = get_contours(thresholded_image)
+cv2.imwrite("contoured.png", contoured_image)
+# while 1:
+#     cv.ShowImage("Vision", image_to_process)
